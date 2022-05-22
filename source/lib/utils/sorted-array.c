4758f20545375d678f6991240902d35e21deef71
Vincent Wei <vincent@minigui.org>
2022-05-22 11:52:50 +0800

    add author

--- source/lib/utils/sorted-array.c
+++ source/lib/utils/sorted-array.c
@@ -3,6 +3,8 @@
  *
  * Copyright (C) 2021 FMSoft <https://www.fmsoft.cn>
  *
+ * Author: Vincent Wei <https://github.com/VincentWei>
+ *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
